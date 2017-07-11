3919bdeaba0b2c03d01c93ec4582a1271098561f
Jackson Donovan <iinventedtwitter@gmail.com>
2017-07-11 13:12:51 -0600

    attempting to fix deployment problem.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieSession({
 }))
 
 // app.use(express.static(__dirname + '../views'))
-app.use(express.static(__dirname + '../public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname, 'server')));
 
